07cd33b1e6830a84f7d4e7fca0d122ffaa85981a
Gustavo Schade <126472320+guschade@users.noreply.github.com>
2023-05-16 20:21:31 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 function converter() {
-    var fahrenheit = document.getElementById("fahrenheit").value;
-    var celsius = (fahrenheit - 32) * 5 / 9;
-    alert(fahrenheit + "°F equivale a " + celsius.toFixed(2) + "°C.");
-    var resultado = document.getElementById("resultado");
-        resultado.innerHTML = fahrenheit + "°F equivale a " + celsius.toFixed(2) + "°C.";
-        resultado.style.display = "block";
-    if (celsius >= -273){
-      if (celsius > 30) {
-          document.body.style.backgroundColor = "orange";
-      } else {
-          document.body.style.backgroundColor = "lightblue";
-      }
-      
-    }  
-    
+  var fahrenheit = document.getElementById("fahrenheit").value;
+  var celsius = ((fahrenheit - 32) * 5) / 9;
+  var resultado = document.getElementById("resultado");
+  resultado.innerHTML =
+    fahrenheit + "°F equivale a " + celsius.toFixed(2) + "°C.";
+  resultado.style.display = "block";
+  if (celsius >= -273) {
+    if (celsius > 30) {
+      document.body.style.backgroundColor = "orange";
+    } else {
+      document.body.style.backgroundColor = "lightblue";
+    }
+  }
 }
+
+var input = document.getElementById("fahrenheit");
+input.addEventListener("keydown", function (event) {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    converter();
+  }
+});
